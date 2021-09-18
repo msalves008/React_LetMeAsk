725db0f46364236c6142129be98e2ce064a58d72
msalves@008 <matheusdesousaalves2018@gmail.com>
2021-09-18 01:11:53 -0300

    finalizando hooks de autenticaçao

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,23 @@
+
 import { useHistory } from 'react-router'
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
 import '../styles/auth.scss'
 import {Button} from '../components/Button';
-import {auth, firebase} from '../services/firebase'
+import { useAuth } from '../hooks/userAuth'
+
 
 export function Home(){
 
   const history = useHistory();
+  const {user, signInWithGoogle} = useAuth();
 
-  function handleCreateRoom(){
-    const provader = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithPopup(provader).then(result => {
-      console.log(result)
-      history.push('/rooms/new');
-    })
+  async function handleCreateRoom(){
+    if(!user){
+      await signInWithGoogle();
+    }
+    history.push('/rooms/new');
   }
 
   return(
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,11 +1,12 @@
 import {Link} from 'react-router-dom'
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
-
 import '../styles/auth.scss'
 import {Button} from '../components/Button';
+import { useAuth } from '../hooks/userAuth';
 
 export function NewRoom(){
+  const {user} = useAuth();
   return(
     <div id="page-auth">
       <aside>
@@ -14,7 +15,7 @@ export function NewRoom(){
         <p>Tire as dúvidas da sua audiência em tempo-real</p>
       </aside>
 
-      <main>
+      <main>    
         <div className="main-content">
           <img src={logoImg} alt="logo" />
           <h2>Criar uma nova sala</h2>
