54ceea5b5b18a74c6b17c6e8fb2965cbc6056168
msalves@008 <matheusdesousaalves2018@gmail.com>
2021-09-19 16:23:57 -0300

    adicionando funcionalidade para encerrar sala

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -35,6 +35,10 @@ export function Home(){
       alert('Room does not exist');
       return;
     }
+    if(roomRef.val().endedAt){
+      alert('Room already closed');
+      return;
+    }
     history.push(`/rooms/${roomCode}`);
 
   }
--- src/pages/adminRom.tsx
+++ src/pages/adminRom.tsx
@@ -1,5 +1,5 @@
 
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 
 import logoImg from "../assets/images/logo.svg";
 import deleteImg from "../assets/images/delete.svg"
@@ -19,11 +19,19 @@ type RoomParams = {
 };
 
 export function AdminRoom() {
+  const history = useHistory();
   //const { user } = useAuth();
   const params = useParams<RoomParams>();
 
   const roomId = params.id;
   const {title, questions} = useRoom(roomId);
+
+  async function handleEndRoom(){
+    await database.ref(`rooms/${roomId}`).update({
+      endedAt: new Date()
+    })
+    history.push('/')
+  }
 
   async function handleDeleteQuestion(questionId: string) {
    if(window.confirm('Tem certeza que você deseja excluir esta pergunta?')){
@@ -38,7 +46,7 @@ export function AdminRoom() {
           <img src={logoImg} alt="Letmeask" />
           <div>
           <RoomCode code={roomId} />
-          <Button isOutlined>Encerrar Sala</Button>
+          <Button isOutlined onClick={handleEndRoom}>Encerrar Sala</Button>
           </div>
         </div>
       </header>
